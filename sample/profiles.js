c46a53b357ca56b47f5e7f8b6a673d176906f066
Luis Felipe <luisfl92@hotmail.com>
2018-08-02 17:10:08 -0500

    Update profiles.js

--- sample/profiles.js
+++ sample/profiles.js
@@ -1,1 +1,13 @@
-hh
+////
+// @author Tony McGuckin, IBM
+// @name Profiles Customization
+// @version 0.1
+// @date February, 2017
+//
+if(typeof(dojo) != "undefined") {
+    dojo.place(
+        "<link rel=\"stylesheet\" type=\"text/css\" href=\"/files/Chefcompanycustomizer/samples/profiles/profilesCustomization.css?repoName=customizer\"></link>",
+        dojo.doc.head,
+        "last"
+    );
+}
